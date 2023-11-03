36ea6fd3e7c99f4ef1c70b1d0d84f059155e24f1
wx15251 <wx15251@gmail.com>
2023-11-03 18:05:20 -0400

    test for toString position3D

--- Test/ReadOnlyReversiModelTests.java
+++ Test/ReadOnlyReversiModelTests.java
@@ -1,3 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.swing.text.Position;
+
+import model.Position3D;
+
 public class ReadOnlyReversiModelTests {
   //TODO: Make tests for the read only methods to make sure they return the right stuff and
   //can't mutate the model
@@ -5,4 +12,10 @@ public class ReadOnlyReversiModelTests {
   //TODO: test isMoveLegal thoroughly (can just use prior examples)
 
   //TODO: test hasLegalMove
+
+  @Test
+  public void testPosition3DToString() {
+    Position3D pos = new Position3D(-3, -2, 5);
+    Assert.assertEquals("-3, -2, 5", pos.toString());
+  }
 }
