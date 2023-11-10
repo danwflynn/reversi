d5ad225aca5d27b77e9cdb3662cecc521d69b9a0
wx15251 <wx15251@gmail.com>
2023-11-09 21:05:33 -0500

    JavaDoc for Reversi.java

--- src/Reversi.java
+++ src/Reversi.java
@@ -2,6 +2,9 @@ import model.*;
 import view.IGraphicalView;
 import view.ReversiGraphicalView;
 
+/**
+ * The main class for running a game of Reversi.
+ */
 public final class Reversi {
   public static void main(String[] args) {
     IReversiModel model = new ReversiModelImpl(2);
