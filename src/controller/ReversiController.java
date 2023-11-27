f946660d7dab17a2359a148c8fe984dade02a6f7
wx15251 <wx15251@gmail.com>
2023-11-26 21:52:07 -0500

    Comments for controller interface

--- src/controller/ReversiController.java
+++ src/controller/ReversiController.java
@@ -2,7 +2,19 @@ package controller;
 
 import model.Position3D;
 
+/**
+ * An interface for the controller for a game of Reversi, allowing a player to operate the game.
+ */
 public interface ReversiController {
+
+  /**
+   * Passes the current player's turn without placing a tile.
+   */
   void pass();
+
+  /**
+   * Places a tile at the given Position3D.
+   * @param pos The position to place at
+   */
   void placeTile(Position3D pos);
 }
