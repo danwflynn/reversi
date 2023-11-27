fbaa0c77c1494b950abbb31573e06234b0b7e4c2
wx15251 <wx15251@gmail.com>
2023-11-26 21:55:03 -0500

    Comments for controller impl

--- src/controller/ReversiControllerImpl.java
+++ src/controller/ReversiControllerImpl.java
@@ -5,11 +5,21 @@ import model.Player;
 import model.Position3D;
 import view.IGraphicalView;
 
+/**
+ * An implementation for the controller of a Reversi game, which allows a player to operate the
+ * game.
+ */
 public class ReversiControllerImpl implements ReversiController {
   private IReversiModel model;
   private Player player;
   private IGraphicalView view;
 
+  /**
+   * Constructs a ReversiControllerImpl.
+   * @param model The model to base the controller on
+   * @param player The player using this controller
+   * @param view The view that the player is able to see
+   */
   public ReversiControllerImpl(IReversiModel model, Player player, IGraphicalView view) {
     this.model = model;
     this.player = player;
@@ -17,11 +27,18 @@ public class ReversiControllerImpl implements ReversiController {
     this.view.addObserver(this);
   }
 
+  /**
+   * Passes the current player's turn without placing a tile.
+   */
   @Override
   public void pass() {
 
   }
 
+  /**
+   * Places a tile at the given Position3D.
+   * @param pos The position to place at
+   */
   @Override
   public void placeTile(Position3D pos) {
     model.placeTile(pos);
